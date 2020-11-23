b8f743ddfa47e4f1975ce46a1da2df8b327417a4
Andrew_Pav <igorjizn@mail.ru>
2020-11-23 13:41:27 +0300

    PAA_Task_02

---
+++ PAA_Task_02/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PAA_Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string exit = "";
+            while("exit" != exit)
+            {
+                Console.WriteLine("Чтобы выйти из программы введите \"exit\" ");
+                exit = Console.ReadLine();
+            }
+        }
+    }
+}
